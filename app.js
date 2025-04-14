8628e5ac878533dad0d1b7195d239595956d6ebf
enderekim <you@example.com>
2025-04-14 13:56:50 +0300

    Parameter Added

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const readline = require('readline');
 
-function sayHello() {
+function sayHello(Name) {
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
 
-    rl.question("What is your name? ", (name) => {
-        console.log("Hello " + name);
+    rl.question("What is your name? "  , (name) => {
+        console.log("Hello " + Name + " " + name);
         rl.close();
     });
 }
 
 // Call the function
-sayHello();
+sayHello("Ender");
